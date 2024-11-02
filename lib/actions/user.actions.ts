f525c5d9218d102679ca30ae605d6327c13684dc
Safak Simsek <safaksimsek05@gmail.com>
2024-11-02 13:04:22 -0700

    implement authform for signin/signup

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,19 @@
+'use server'
+
+export const signIn = async () => {
+    try {
+
+        // mutation / database / makefetch
+    } catch (error) {
+        console.error('Error', error)
+    }
+}
+
+export const singUp = async () => {
+    try {
+        // mutation / database / makefetch
+    
+    } catch (error) {
+        console.error('Error', error)
+    }
+}
